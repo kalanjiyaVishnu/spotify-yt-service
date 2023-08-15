e224ddfcd9102798198fbf53186d6730329baaed
kalanjiyaVishnu <kalanjiya.vishnu01@gmail.com>
2023-08-15 09:56:09 +0530

    Refactor download utils with query as input

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -2,6 +2,21 @@ import fs from "fs"
 import path from "path"
 import youtubeDl, { YtFlags } from "youtube-dl-exec"
 import { BASE_DIR } from "../configs"
+import axios from "axios"
+
+const YOUTUBE_KEY = "AIzaSyCSR7EH9xhoAZMTYEhaWtNOSzu-lG9eraU"
+const BASE_YT_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
+
+async function getYoutubeSearchId(query: string) {
+  const params = {
+    part: "id",
+    type: "video",
+    q: query,
+    key: YOUTUBE_KEY,
+  }
+  const res = await axios.get(BASE_YT_SEARCH_URL, { params })
+  return res.data.items[0].id.videoId
+}
 
 const getInfo = (url: string, flags?: YtFlags) =>
   youtubeDl(url, { dumpSingleJson: true, ...flags })
@@ -9,7 +24,9 @@ const getInfo = (url: string, flags?: YtFlags) =>
 const fromInfo = (infoFile: string, flags: YtFlags) =>
   youtubeDl.exec(infoFile, { loadInfoJson: infoFile, ...flags })
 
-async function downloadWithUrl(url: string, ext = "mp3") {
+async function download(query: string, ext = "mp3") {
+  const id = await getYoutubeSearchId(query)
+  const url = `https://www.youtube.com/watch?v=${id}`
   const info = await getInfo(url)
 
   const current_url = new URL(url)
@@ -38,4 +55,4 @@ async function downloadWithUrl(url: string, ext = "mp3") {
   } as YtFlags)
 }
 
-export { downloadWithUrl }
+export { download }
