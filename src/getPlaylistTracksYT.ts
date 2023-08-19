2629d23867282f2f382a6d4fc34b6cf8a19c7f64
kalanjiyaVishnu <kalanjiya.vishnu01@gmail.com>
2023-08-19 22:50:56 +0530

    Handle Error

--- src/getPlaylistTracksYT.ts
+++ src/getPlaylistTracksYT.ts
@@ -1,25 +1,25 @@
+import fs from "fs"
 import { get, isArray } from "lodash"
 import path from "node:path"
 import { download } from "./utils/download"
 
 export async function getPlaylistTracksYT(userId: string) {
-  try {
-    const publicDir = path.resolve(__dirname, "../public")
-    const playlistPath = `${publicDir}/${userId}.json`
-    const userData: Object[] = (await import(playlistPath)).default
+  const publicDir = path.resolve(__dirname, "../public")
+  const playlistPath = `${publicDir}/${userId}.json`
 
-    if (!userData || !isArray(userData)) return
+  if (!fs.existsSync(playlistPath)) throw new Error("User not found")
 
-    const downloadPromises = userData.slice(0, 2).flatMap((playlist) =>
-      get(playlist, "tracks", [])
-        .slice(0, 1)
-        .map((track: any) =>
-          download(`${get(track, "trackName")} ${get(track, "by")}`)
-        )
-    )
-    const resolvedPromises = await Promise.all(downloadPromises)
-    return resolvedPromises.length
-  } catch (error) {
-    return error
-  }
+  const userData: Object[] = (await import(playlistPath)).default
+
+  if (!userData || !isArray(userData)) throw new Error("Invalid playlist")
+
+  const downloadPromises = userData.slice(0, 2).flatMap((playlist) =>
+    get(playlist, "tracks", [])
+      .slice(0, 1)
+      .map((track: any) =>
+        download(`${get(track, "trackName")} ${get(track, "by")}`)
+      )
+  )
+  const resolvedPromises = await Promise.all(downloadPromises)
+  return resolvedPromises.length
 }
